f431ee5558fd197bd728e2d205122be4692f091e
Ryan Workman <rdavid1099@gmail.com>
2017-12-13 15:51:24 -0700

    Add error if given robot name is not found in data

--- robot_name.sh
+++ robot_name.sh
@@ -23,17 +23,26 @@ save_names_metadata() {
 }
 
 print_meta() {
+  name_found=1
   while read bot; do
     ROBOT_INFO=""
     [[ "$(echo $bot | cut -d ";" -f 1)" = "$1" || "all" = "$1" ]] && format_robot_info bot
-    [[ "$ROBOT_INFO" != "" ]] && echo $ROBOT_INFO
+    if [[ "$ROBOT_INFO" != "" ]]; then
+      echo $ROBOT_INFO
+      name_found=0
+    fi
   done <.meta.robot_name
+  [[ "$name_found" = 1 && "$1" != "all" ]] && robot_not_found_error
 }
 
 format_robot_info() {
   robot_name=$(echo $bot | cut -d ";" -f 1)
   restart_amt=$(echo $bot | cut -d ";" -f 2)
   ROBOT_INFO="ROBOT: $robot_name, TIMES RESTARTED: $restart_amt"
+}
+
+robot_not_found_error() {
+  echo "ERROR: Invalid name given. Could not find data on $1."
 }
 
 case $1 in
--- robot_name_test.sh
+++ robot_name_test.sh
@@ -61,3 +61,12 @@ teardown() {
   [[ "$status" -eq "0" ]]
   [[ "$output" = "ROBOT: $ROBOT_THREE, TIMES RESTARTED: 0" ]]
 }
+
+@test "error is printed if unknown name is passed to script" {
+  ROBOT_ONE=$(bash $PWD/robot_name.sh new)
+
+  run bash robot_name.sh display C3PO
+
+  [[ "$status" -eq "0" ]]
+  [[ "$output" = "ERROR: Invalid name given. Could not find data on C3PO." ]]
+}
